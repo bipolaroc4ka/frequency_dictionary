eff7851055eeffa493d048b34be893619a8ba9b1
unknown <sychev.dak@yandex.ru>
2021-08-23 10:57:03 +0300

    Частотный словарь

---
+++ Exam_Tests/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Exam_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter dir path: ");
+            string path = Console.ReadLine();            
+            string[] files = Directory.GetFiles(path);
+            Dictionary<string, int> vacabulary = new Dictionary<string, int>();
+            char[] separator = { ' ', ',', '.', '!', '?', ':' };
+            foreach (string file in files)
+            {
+                if (file.Contains(".txt"))
+                {
+                   string[] text = File.ReadAllLines(file);
+                    foreach (string tex in text)
+                    {
+                        string[] www = tex.Split(separator);
+                        foreach (var item in www)
+                        {
+                            if (vacabulary.ContainsKey(item))
+                            {
+                                vacabulary[item]++;
+                            }
+                            else
+                            {
+                                vacabulary.Add(item, 1);
+                            }
+                        }
+                    }
+                }
+                
+            }
+            var result = vacabulary.OrderByDescending(i => i.Value);
+            Console.WriteLine("Enter path for file result: ");
+            string fileresult = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(fileresult + "\\result.txt");
+            foreach (var item in result)
+            {
+               byte[] array = System.Text.Encoding.Default.GetBytes(item.Key + " " +item.Value);
+
+                sw.WriteLine(item.ToString());
+                
+               
+                //Console.WriteLine(item);
+            }
+            sw.Close();
+            Console.WriteLine("Complete!");
+
+
+        }
+    }
+}
---
+++ Exam_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Exam_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exam_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1ac54f21-88bd-4f92-9dff-ebb4d3322a1a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
